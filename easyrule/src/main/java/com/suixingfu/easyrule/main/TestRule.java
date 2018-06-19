141d90db8479a92cdeef2ae2e7b5e5c4694bfc4a
Monkey-D-GD <35684452+Monkey-D-GD@users.noreply.github.com>
2018-06-19 14:47:27 +0800

    easy_rule demo

---
+++ easyrule/src/main/java/com/suixingfu/easyrule/main/TestRule.java
@@ -0,0 +1,47 @@
+package com.suixingfu.easyrule.main;
+
+import com.suixingfu.easyrule.rule.BlueRule;
+import com.suixingfu.easyrule.rule.RedRule;
+import com.suixingfu.easyrule.rule.YellowRule;
+import org.easyrules.api.RulesEngine;
+import org.easyrules.core.RulesEngineBuilder;
+
+/**
+ * <dl>
+ * <dd>描述: ~节点业务逻辑实现</dd>
+ * <dd>创建时间：  14:44 2018/6/12</dd>
+ * <dd>创建人： guodong</dd>
+ * <dt>版本历史: </dt>
+ * <pre>
+ * Date         Author      Version     Description
+ * ------------------------------------------------------------------
+ * 2018/6/12      guodong       1.0        1.0 Version
+ * </pre>
+ * </dl>
+ */
+public class TestRule {
+
+    public static void main(String[] args) {
+
+        RulesEngine fizzBuzzEngine =
+                RulesEngineBuilder.aNewRulesEngine()
+                        .withSkipOnFirstAppliedRule(true) //如果第一个规则满足条件，后面的规则将不再执行
+                        .withSilentMode(true)
+                        .build();
+
+        RedRule redRule = new RedRule();
+        YellowRule yellowRule = new YellowRule();
+        BlueRule blueRule = new BlueRule();
+
+        fizzBuzzEngine.registerRule(redRule);
+        fizzBuzzEngine.registerRule(yellowRule);
+        fizzBuzzEngine.registerRule(blueRule);
+
+        redRule.setName("zhangsan");
+        yellowRule.setName("zhangsan");
+        blueRule.setName("zhangsan");
+
+        fizzBuzzEngine.fireRules();
+
+    }
+}
---
+++ easyrule/src/main/java/com/suixingfu/easyrule/rule/BlueRule.java
@@ -0,0 +1,45 @@
+package com.suixingfu.easyrule.rule;
+
+import org.easyrules.annotation.Action;
+import org.easyrules.annotation.Condition;
+import org.easyrules.annotation.Priority;
+import org.easyrules.annotation.Rule;
+
+/**
+ * <dl>
+ * <dd>描述: ~节点业务逻辑实现</dd>
+ * <dd>创建时间：  14:31 2018/6/12</dd>
+ * <dd>创建人： guodong</dd>
+ * <dt>版本历史: </dt>
+ * <pre>
+ * Date         Author      Version     Description
+ * ------------------------------------------------------------------
+ * 2018/6/12      guodong       1.0        1.0 Version
+ * </pre>
+ * </dl>
+ */
+@Rule
+public class BlueRule {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Condition
+    public boolean isZhangSan(){
+        return name.equals("zhangsan");
+    }
+
+    @Action
+    public void printHello(){
+        System.out.println("Blue : " + name);
+    }
+
+    @Priority
+    public int getPriority(){
+        return 1;
+    }
+
+}
